691045c86be8976cd22a3592ce5f4ed83dbb5491
Rico <kangll@uw.edu>
2023-02-24 12:30:31 -0800

    Delete bsg_cgol_cell/build directory

--- bsg_cgol_cell/build/syn-rundir/genus.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.12-s068_1, built Mar  4 2022 14:12:46
-
-# Date: Fri Feb 24 11:41:43 2023
-# Host: linux-lab-049.ece.uw.edu (x86_64 w/Linux 3.10.0-1160.81.1.el7.x86_64) (8cores*8cpus*1physical cpu*Intel(R) Core(TM) i7-9700 CPU @ 3.00GHz 12288KB)
-# OS:   CentOS Linux release 7.9.2009 (Core)
-
-source /home/kangll/ee477-designs-final-project/bsg_cgol_cell/build/syn-rundir/syn.tcl
